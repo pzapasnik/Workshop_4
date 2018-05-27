d95bee5455bf367250b3d56ad2c8b0c492b7ed77
pzapasnik <pzapasnik@gmail.com>
2018-05-27 16:45:57 +0200

    kolejne zadania

--- js/app.js
+++ js/app.js
@@ -1,15 +1,8 @@
 $(document).ready(function(){
     var rootDiv = $('#root');
 // zad 3
-    $.ajax({
-        url: 'http://localhost:8282/books/',
-        type: 'GET'
-    }).done(function(data){
-        for (var i = 0; i < data.length; i++) {
-            var newTag = $('<div class="book" data-id="' + data[i].id +'">' + data[i].title + '<div style=display:none;></div></div>');
-            rootDiv.append(newTag);
-        }
-    })
+    refreshBooks(rootDiv);
+    handleForm();
     // zad 4
     rootDiv.on('click', '.book', function(){
         var bookDiv = $(this);
@@ -20,8 +13,52 @@ $(document).ready(function(){
             url:'http://localhost:8282/books/' + bookId,
             type: 'GET'
         }).done(function(bookDetails){
-            detailDiv.show();
+            detailDiv.toggle();
             detailDiv.text('Author: ' + bookDetails.author + ', id: ' + bookDetails.id + ', isbn: ' + bookDetails.isbn + ', publisher ' + bookDetails.publisher + ', type' + bookDetails.type);
         })
     })
+    // zad 5
+
 });
+
+function handleForm() {
+    var form = $('.new_book');
+    var submit = form.find('#add-button');
+    submit.on('click', function(e){
+        e.preventDefault();
+        
+        var newBook = {};
+
+        newBook.author = $('#author').val();
+        newBook.isbn = $('#isbn').val();
+        newBook.publisher = $('#publisher').val();
+        newBook.title = $('#title').val();
+        newBook.type = $('#type').val();
+        
+        $.ajax({
+            url: 'http://localhost:8282/books/',
+            type: 'POST',
+            headers:{
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            data: JSON.stringify(newBook)
+        }).done(function(){
+            refreshBooks($('#root'));
+        })
+    })
+
+}
+// zad 3
+function refreshBooks(rootElement) {
+    rootElement.html('');
+    $.ajax({
+        url: 'http://localhost:8282/books/',
+        type: 'GET'
+    }).done(function(data){
+        for (var i = 0; i < data.length; i++) {
+            var newTag = $('<div class="book" data-id="' + data[i].id +'">' + data[i].title + '<div style=display:none;></div></div>');
+            rootElement.append(newTag);
+        }
+    })
+}
